e5745a9dc3f81370c4f9d6156444dd8cc467a8f9
lraty <lraty@outlook.com>
2024-03-22 11:39:43 +0800

    fix white screen when profile or release mode

--- outerwilds_signalscope/lib/main.dart
+++ outerwilds_signalscope/lib/main.dart
@@ -1,6 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:outerwilds_signalscope/pages/home.dart';
+import 'dart:ui'; //window需要import ui库
 
+// https://www.jianshu.com/p/4f0651241956
+//debug的时候加载比较久？
 void main() {
+  //如果size是0，则设置回调，在回调中runApp
+  //window. 弃用 This feature was deprecated after v3.7.0-32.0.pre 终于要来了吗多窗口
+  final view = PlatformDispatcher.instance.views.first;
+  if (view.physicalSize.isEmpty) {
+    PlatformDispatcher.instance.onMetricsChanged = () {
+      //在回调中，size仍然有可能是0
+      if (!view.physicalSize.isEmpty) {
+        PlatformDispatcher.instance.onMetricsChanged = null;
+        _runApp();
+      }
+    };
+  } else {
+    //如果size非0，则直接runApp
+    _runApp();
+  }
+}
+
+void _runApp() {
   runApp(HomePage());
 }
